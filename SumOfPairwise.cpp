0efaf3006f417e100aa4b79edf41c05092c3a255
shubham <shubham.wheofficial@gmail.com>
2019-07-07 16:42:49 +0530

    Add existing files

---
+++ SumOfPairwise.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int hammingDistance(const vector<int> &A) {
+    int inputSize = A.size();
+    int mod = 1000000007;
+    int sum = 0;
+    for (int bitPosition = 0; bitPosition < 31; bitPosition++) {
+        int cntBitOne = 0, cntBitZero = 0;
+        for(int i = 0; i < inputSize; i++) {
+            if (A[i] & (1 << bitPosition)) cntBitOne++;
+            else cntBitZero++;
+        }
+        sum = sum + ((2LL * cntBitOne * cntBitZero) % mod);
+        if (sum >= mod) sum = sum - mod;
+    }
+    return sum;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    int res=hammingDistance(a);
+    cout<<res<<endl;
+    return 0;
+}
